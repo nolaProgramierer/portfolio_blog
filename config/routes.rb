0ef4d241e688095330fc1407dd6daebf97db64e5
Glenn Langdon <glennlangdon@gmail.com>
2016-09-27 20:07:00 -0500

    Added Article new, error checking and basic validation

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   root 'welcome#new'
   resources :users
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
