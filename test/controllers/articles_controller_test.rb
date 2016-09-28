d75604a386658d25cbd34a1fee67210176427fb0
Glenn Langdon <glennlangdon@gmail.com>
2016-09-28 00:04:18 -0500

    Basic articles testing

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,9 +1,33 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
+
+  def setup
+    @article = Article.new(title: "Title", body: "Now is the time...")
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  # test "should get show" do
+  #   get article_path, params: { id: 4 }
+  #   assert_response :success
+  # end
+
+  test "title should not be too short" do
+    @article.title = "a" * 2
+    assert_not @article.valid?
+  end
+
+  test "body of article should be present" do
+    @article.body = "    "
+    assert_not @article.valid?
+  end
 end
