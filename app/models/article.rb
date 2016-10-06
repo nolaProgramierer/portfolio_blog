b0a5e312843275b3469d4670151a5340ab3e3f0c
Glenn Langdon <glennlangdon@gmail.com>
2016-10-06 18:50:07 -0500

    Add parameterize method to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ActiveRecord::Base
+
+  # add SEO friendly titles to url
+  def to_param
+    "#{id} #{title}".parameterize
+  end
+  
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true
 end
