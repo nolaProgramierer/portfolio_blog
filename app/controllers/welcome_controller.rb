0be6bca7a60be28e492c5cb2c50ac000e0e10ef6
Glenn Langdon <glennlangdon@gmail.com>
2016-12-22 22:02:24 -0600

    Added sort to welcome index controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
-  
+  include WelcomeHelper
+
   def index
     @articles = Article.all
+    @articles = @articles.sort { |x, y| y.created_at <=> x.created_at }
   end
 end
