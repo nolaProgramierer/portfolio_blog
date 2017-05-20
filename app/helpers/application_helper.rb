7201e3ed2305d80eef147ffeebb8bafc8a74eece
Glenn Langdon <glennlangdon@gmail.com>
2017-05-20 12:56:34 -0500

    Changed base title of blog site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "Glenn's Ruby/Rails blog"
+    base_title = "A Ruby and Rails blog"
     if page_title.empty?
       base_title
     else
